8e2af27e46420c5a06ef34c2ad693c1b2ab62337
Luan Gustavo <gluan1213@gmail.com>
2025-04-21 23:22:30 -0300

    feat: add toggle mobile menu

--- script/script.js
+++ script/script.js
@@ -1,12 +1,19 @@
-let current_theme = localStorage.getItem("theme") || "light";
+let current_theme = localStorage.getItem('theme') || 'light';
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.body.setAttribute("data-theme", current_theme);
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.setAttribute('data-theme', current_theme);
 });
 
-document.getElementById("toggle-theme").addEventListener("click", () => {
-    current_theme = current_theme === "light" ? "dark" : "light";
-    localStorage.setItem("theme", current_theme);
+document.getElementById('toggle-theme').addEventListener('click', () => {
+    current_theme = current_theme === 'light' ? 'dark' : 'light';
+    localStorage.setItem('theme', current_theme);
 
-    document.body.setAttribute("data-theme", current_theme);
+    document.body.setAttribute('data-theme', current_theme);
+});
+
+document.getElementById('toggle-mobile-menu').addEventListener('click', () => {
+    const menu = document.getElementById('menu-items');
+
+    let isActive = menu.classList.contains('active');
+    menu.classList.toggle('active', !isActive);
 });
